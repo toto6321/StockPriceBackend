188a55aa179d2bed2829808bf7cc889b89e38e61
TO <HUTO.icu@outlook.com>
2021-08-19 02:49:53 +0800

    FEATURE: give them distinct name

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,5 @@
-docker build -f Dockerfile -t my-app-backend .
-docker build -f Dockerfile-mysql -t my-mysql .
+docker build -f Dockerfile-mysql -t my-mysql-img .
+docker build -f Dockerfile -t my-app-backend-img .
+
+docker container run --name my-mysql-con my-mysql-img
+docker container run --name my-app-backend-con my-app-backend-img
