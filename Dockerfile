061347afaa7e760cd7c9574d42cde3d47e560851
TO <HUTO.icu@outlook.com>
2021-08-19 12:55:26 +0800

    FEATURE: fix scripts

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,11 @@ WORKDIR /app
 
 ADD target/Group777777-0.0.1-SNAPSHOT.jar app.jar
 
-EXPOSE 8080
+EXPOSE 8081
 
-RUN sh -c "echo spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver >> application.properties"
-RUN sh -c "echo spring.datasource.url=jdbc:mysql://mysql:3306/stock_market >> application.properties"
+RUN sh -c "echo spring.datasource.url=jdbc:mysql://localhost:3306/stock_market >> application.properties"
 RUN sh -c "echo spring.datasource.username=root >> application.properties"
 RUN sh -c "echo spring.datasource.password=replace_with_your_password >> application.properties"
-RUN sh -c "echo spring.jpa.hibernate.ddl-auto=update >> application.properties"
-RUN sh -c "echo spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect >> application.properties"
-RUN sh -c "echo spring.jackson.serialization.FAIL_ON_EMPTY_BEANS=false >> application.properties"
+RUN sh -c "echo server.port=8081 >> application.properties"
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- create_db.sql
+++ create_db.sql
@@ -6,24 +6,13 @@ USE `stock_market`;
 -- ------------------------------------------------------
 -- Server version	8.0.26
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
 --
 -- Table structure for table `stock_price`
 --
 
 -- DROP TABLE IF EXISTS `stock_price`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE IF NOT EXISTS `stock_price`
 (
     `id`     bigint       NOT NULL AUTO_INCREMENT,
@@ -47,8 +36,7 @@ CREATE TABLE IF NOT EXISTS `stock_price`
 --
 
 -- DROP TABLE IF EXISTS `company`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE IF NOT EXISTS `company`
 (
     `id`       bigint       NOT NULL AUTO_INCREMENT,
@@ -62,4 +50,4 @@ CREATE TABLE IF NOT EXISTS `company`
     AUTO_INCREMENT = 64
     DEFAULT CHARSET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
