36804b94fc060e7cd96ee573f638652f491e2c5f
root <root@MW.localdomain>
2021-08-26 18:20:21 +0800

    FEATURE: add CORS

--- src/main/java/com/citi/group77777/controller/RootController.java
+++ src/main/java/com/citi/group77777/controller/RootController.java
@@ -1,9 +1,11 @@
 package com.citi.group77777.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class RootController {
